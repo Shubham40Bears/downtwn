3273d6e8267f34f26bd5209ef214259c3c92575c
Shubham <apple@Shubham.local>
2025-02-13 21:55:42 +0530

    adding cat order

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -10,7 +10,7 @@ class LandingPageController extends Controller
 {
     public function index()
     {
-        $categories = Category::with('products')->get();
+        $categories = Category::with('products')->orderBy('created_at', 'desc')->get();
         return view('home', compact('categories'));
     }
 }
