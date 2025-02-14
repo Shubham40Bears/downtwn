9b57d50a402bad8b91b6b676619be173cd09a5f6
Shubham <apple@Shubham.local>
2025-02-14 20:17:12 +0530

    hotjar data

--- resources/views/layouts/downtwn.blade.php
+++ resources/views/layouts/downtwn.blade.php
@@ -43,6 +43,19 @@
     src="https://www.facebook.com/tr?id=469856359523154&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Meta Pixel Code -->
+     <!-- heatmap start -->
+     <script>
+    (function (c, s, q, u, a, r, e) {
+        c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+        c._hjSettings = { hjid: a };
+        r = s.getElementsByTagName('head')[0];
+        e = s.createElement('script');
+        e.async = true;
+        e.src = q + c._hjSettings.hjid + u;
+        r.appendChild(e);
+    })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5305127);
+</script>
+     <!-- heatmap end -->
 </head>
 <body>
     <div id="app">
